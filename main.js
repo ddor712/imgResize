17154041bb06d5aaf46f432198358784cdf65521
ddor712 <dannydor712@gmail.com>
2023-07-17 15:21:32 -0500

    Added the entire functionality to the program. Also added the html and css files for the styling of the app. The image resizer is able to take the input of a img file and resize it to the set height and width given by the user. It will then be transferred into a new folder on their computer that will have all of their resized images. Used a lot of electron and node but also included Toastify for alerts. Will only need to make some design changes now but the overall app is done and it is functional

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const path = require('path')
-const {app, BrowserWindow, Menu} = require('electron');
+const os = require('os');
+const fs = require ('fs');
+const resizeImg = require('resize-img');
+const {app, BrowserWindow, Menu, ipcMain, shell} = require('electron');
 
-const isDev = process.env.NODE_ENV !== 'development';
+process.env.NODE_ENV = 'production';
+
+const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin'; // is true or false
+
+let mainWindow;
+let aboutWindow;
 
 // Creates the Main window
 function createMainWindow(){
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'Image Resizer',
         width:isDev ? 1200 : 800,
-        height:600
+        height:600,
+        webPreferences: {
+            contextIsolation:true,
+            nodeIntegration:true,
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     // Open devtools if in dev env
@@ -22,7 +35,7 @@ function createMainWindow(){
 
 // Create About Window
 function createAboutWindow(){
-    const aboutWindow = new BrowserWindow({
+    aboutWindow = new BrowserWindow({
         title: 'About Image Resizer',
         width:300,
         height:600
@@ -37,6 +50,9 @@ app.whenReady().then(() => {
     // Implement Menu
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
+
+    // Remove mainWindow from memory on close
+    mainWindow.on('closed', () => (mainWindow = null))
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
@@ -62,6 +78,41 @@ const menu = [
         }]
     }] : [])
 ];
+
+// Respond to ipcRenderer resize
+ipcMain.on('image:resize', (e,options) => {
+    options.dest = path.join(os.homedir(), 'imageresizer');
+    resizeImage(options)
+});
+
+// Resize the Image
+async function resizeImage({ imgPath, width, height, dest }){
+    try{
+        const newPath = await resizeImg(fs.readFileSync(imgPath),{
+            width:+width,
+            height:+height
+        })
+
+        const filename = "rs_" + path.basename(imgPath);
+
+        // Create dest folder if it doesnt exist
+        if(!fs.existsSync(dest)){
+            fs.mkdirSync(dest);
+        }
+
+        // Write file to dest
+        fs.writeFileSync(path.join(dest, filename), newPath);
+
+        // Send Success Message Back
+        mainWindow.webContents.send('image:done');
+        
+        // Open the dest folder so they can see the image
+        shell.openPath(dest);
+
+    }catch(err){
+        console.log(err);
+    }
+}
 
 app.on('window-all-closed', () => {
     if (!isMac) app.quit()
